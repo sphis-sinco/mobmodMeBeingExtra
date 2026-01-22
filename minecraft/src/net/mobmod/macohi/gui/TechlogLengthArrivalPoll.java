bb59b8b2838747b70c2277c9bff2b2361ab8adc7
Macohi <sinconsistencia@gmail.com>
2026-01-22 16:44:59 -0600

    finalized screen

--- minecraft/src/net/mobmod/macohi/gui/TechlogLengthArrivalPoll.java
+++ minecraft/src/net/mobmod/macohi/gui/TechlogLengthArrivalPoll.java
@@ -2,6 +2,7 @@ package net.mobmod.macohi.gui;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiSmallButton;
 
 public final class TechlogLengthArrivalPoll extends GuiScreen {
 	private GuiScreen prevGui;
@@ -12,11 +13,18 @@ public final class TechlogLengthArrivalPoll extends GuiScreen {
 
 	public final void initGui() {
 		this.controlList.clear();
-		this.controlList.add(new GuiButton(0, this.width / 2 - 150, this.height / 4 + 48, "Longer, less frequent"));
-		this.controlList.add(new GuiButton(1, this.width / 2 - 50, this.height / 4 + 48, "Shorter, more frequent"));
+		
+		this.controlList.add(new GuiSmallButton(1, this.width / 2 - 200, this.height / 4 + 48, "Longer, less frequent"));
+		this.controlList.add(new GuiSmallButton(1, this.width / 2 + 50, this.height / 4 + 48, "Shorter, more frequent"));
+		
+		this.controlList.add(new GuiButton(0, this.width / 2 - 100, this.height / 4 + 48 + (24 * 4), "Back"));
 	}
 
 	protected final void actionPerformed(GuiButton var1) {
+		if (var1.id == 0)
+		{
+			this.mc.displayGuiScreen(this.prevGui);
+		}
 	}
 
 	public final void drawScreen(int var1, int var2) {
